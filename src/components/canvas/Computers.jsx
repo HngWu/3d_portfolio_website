5fd2907fe37ad57eea69157b1193d54ca16ec79a
HngWu <hngwutan@gmail.com>
2024-11-04 19:15:13 +0800

    feat: Portfolio website

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,67 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+
+  const { scene } = useGLTF("./desktop_pc/scene.gltf");
+
+  // Add error handling around useGLTF
+  try {
+    return (
+      <mesh>
+        <hemisphereLight intensity={1.55} groundColor="black" />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+        <pointLight intensity={1} />
+        <primitive
+          object={scene}
+          scale={isMobile ? 0.7 : 0.75}
+          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+      
+    );
+  } catch (error) {
+    console.error("Error loading GLTF:", error);
+    return null; // Return null or alternative content in case of error
+  }
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+
+
+export default Computers;
+export { ComputersCanvas };
